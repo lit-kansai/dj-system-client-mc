0dc770343bc2e30080e1328a547ba0a4fe8b3b69
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-11-24 05:51:52 +0900

    [setup] formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,16 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
+    '@nuxtjs',
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
-  rules: {}
+  rules: {},
 }

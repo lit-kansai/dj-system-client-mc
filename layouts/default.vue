abf2e6dc801b2f4bac1f1f2738d1497b15697503
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-12-01 02:45:50 +0900

    [add] default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen">
+    <Header />
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  layout: 'default',
+})
+export default class DefaultLayout extends Vue {}
+</script>

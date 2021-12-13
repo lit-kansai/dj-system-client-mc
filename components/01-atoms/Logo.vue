eddc4139dd6e49bf9ca6c5af157202ca4ecb3aa3
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-12-13 22:33:34 +0900

    [add] Logo component

---
+++ components/01-atoms/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    src="~assets/img/header_icon.png"
+    width="78px"
+    height="78px"
+    alt="header_icon.png"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class Logo extends Vue {}
+</script>

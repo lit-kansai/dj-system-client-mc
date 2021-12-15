352d5525118ff4daf4f4394819d382749d6a1135
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-12-15 22:51:17 +0900

    [add] RequestMusicOverview component

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="p-11 flex">
-    <RoomLogo v-bind="roomLogo" />
-    <Card v-bind="card" />
+  <div>
+    <div class="p-11 flex">
+      <RoomLogo v-bind="roomLogo" />
+      <Card v-bind="card" />
+    </div>
+    <div class="p-11">
+      <RequestMusicOverview v-bind="requestMusicOverview" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from '@vue/composition-api'
+import { RequestMusicOverview } from '../components/02-molecules/RequestMusicOverview.vue'
 import { Card } from '~/components/02-molecules/Card.vue'
 import { RoomLogo } from '~/components/02-molecules/RoomLogo.vue'
 
@@ -23,9 +29,16 @@ export default defineComponent({
       roomName: 'ルーム名',
       imageUrl: '',
     })
+    const requestMusicOverview = ref<RequestMusicOverview>({
+      musicTitle: 'ヒステリックナイトガール',
+      artistName: 'PSYQUI',
+      imageUrl:
+        'https://i.scdn.co/image/ab67616d00001e021a8bba168d85553d9b2d47a9',
+    })
     return {
       card,
       roomLogo,
+      requestMusicOverview,
     }
   },
 })

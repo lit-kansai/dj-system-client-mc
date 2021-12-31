e89203ca565346aa60fcc24e412f95750236c333
Yo Higashida <app.y.higashida@gmail.com>
2021-12-31 16:06:46 +0900

    add: header-text

---
+++ components/01-atoms/HeaderText.vue
@@ -0,0 +1,10 @@
+<template>
+  <p class="mt-5 text-4xl text-neon-pink drop-shadow-neon-pink">DJ GASSI</p>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {},
+})
+</script>

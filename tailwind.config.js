f752012e15acd8a6518637b71007a8fad3d0134d
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-01-02 04:27:38 +0900

    [update] preview environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,17 @@ const generateNeon = (color) => {
   return `0 0 0rem #fff, 0 0 .2rem #fff, 0 0 2rem ${color}, 0 0 0.8rem ${color}, 0 0 2.8rem ${color}, inset 0 0 1.3rem ${color};`
 }
 
+const generateModalNeon = (color) => {
+  return `0 0 0rem #fff, 0 0 .2rem #fff, 0 0 2rem ${color}, 0 0 1rem ${color}, inset 0 0 1.3rem ${color};`
+}
 const neonBlue = '#3E65F4'
 const neonPink = '#DD6BDA'
 const neonGreen = '#C8FF7E'
+const yellow = '#FFD768'
 const darkBlue = '#040B15'
 
 module.exports = {
-  // mode: 'jit',
+  mode: 'jit',
   purge: [
     './components/**/*.{vue,js}',
     './constants/**/*.ts',
@@ -29,6 +33,7 @@ module.exports = {
       'neon-blue': neonBlue,
       'neon-pink': neonPink,
       'neon-green': neonGreen,
+      yellow,
       'dark-blue': darkBlue,
     },
     boxShadow: {
@@ -36,6 +41,8 @@ module.exports = {
       'neon-pink': generateNeon(neonPink),
       'neon-green': generateNeon(neonGreen),
       'card-neon-green': '0 0 0.2rem #fff, 0 0 0.8rem #c8ff7e',
+      'modal-neon-pink': generateModalNeon(neonPink),
+      'modal-neon-blue': generateModalNeon(neonBlue),
       'text-input-neon-blue':
         '0 0 0.8rem #3E65F4, 0 0 0.4rem #3E65F4, inset 0 0 0.3rem #3E65F4',
     },
@@ -49,6 +56,7 @@ module.exports = {
     },
   },
   variants: {
+    opacity: ['hover'],
     extend: {},
   },
   plugins: [],

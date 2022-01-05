826767820f17aa48738924736a24f6acdad40be0
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-01-05 19:48:39 +0900

    [add] pages

---
+++ pages/mc/admin/music/spotify-login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>/admin/music/spotify-music-login</h1>
+    <NuxtLink to="/top">top</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
---
+++ pages/room/_id/settings/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>mc/room/:id/settings</h1>
+    <p>{{ id }}</p>
+    <NuxtLink to="/top">top</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const id = $nuxt.$route.params.id
+    return { id }
+  },
+})
+</script>

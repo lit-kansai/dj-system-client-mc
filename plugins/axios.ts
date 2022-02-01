119d5337f9648332e2cb8c959b45ea0236d7f476
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-02-01 23:23:52 +0900

    [modify] axios plugin

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,7 +1,13 @@
 import { Context } from '@nuxt/types'
+import { CamelConverter } from '~/utils/snakeToCamel'
 
 export default function ({ $axios }: Context) {
   $axios.onRequest((config) => {
     console.log(config.url)
+  })
+
+  $axios.onResponse((response) => {
+    response.data = CamelConverter.convert(response.data)
+    return response
   })
 }

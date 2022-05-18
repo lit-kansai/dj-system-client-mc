db8e3a3829bfec33da2f252e91fe6a4efc4188e0
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-05-18 12:11:41 +0900

    [add] nuxtInstance

---
+++ plugins/nuxtInstance.ts
@@ -0,0 +1,6 @@
+import { Context } from '@nuxt/types'
+import { initializeNuxtInstance } from '~/utils/nuxtInstance'
+
+export default function (context: Context) {
+  initializeNuxtInstance(context)
+}

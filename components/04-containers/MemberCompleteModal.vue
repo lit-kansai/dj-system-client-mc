c66f65834e07b780bb184b78f1882bd64753fb9e
Yo Higashida <y.higashida0226+app@gmail.com>
2022-06-08 00:47:50 +0900

    add: complate-modal

---
+++ components/04-containers/MemberCompleteModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-10 flex items-center justify-center">
+    <div>
+      <img src="~assets/img/check.svg" class="h-40 m-auto mb-5" />
+      <h1 class="text-neon-green text-4xl mb-3 text-center">送信完了</h1>
+      <p class="text-center">流れるのをお楽しみに！</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {},
+})
+</script>

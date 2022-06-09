237d514cbcfdddcc0e548f49ecaad019ebe5b4bf
Yo Higashida <y.higashida0226+app@gmail.com>
2022-06-09 13:23:41 +0900

    add: margin

--- components/01-atoms/Container.vue
+++ components/01-atoms/Container.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full h-full p-5 border-2 border-neon-blue shadow-modal-neon-blue rounded-2xl"
+    class="w-full h-full p-2.5 md:p-5 border-2 border-neon-blue shadow-modal-neon-blue rounded-2xl"
   >
     <div
       class="bg-gray-800 border-2 border-neon-pink shadow-modal-neon-pink rounded-2xl"

a7fd1788531940da22a570d85ce0985f04ee75e6
Yo Higashida <y.higashida0226+app@gmail.com>
2022-06-16 00:21:18 +0900

    add: room

--- pages/mc/index.vue
+++ pages/mc/index.vue
@@ -1,14 +1,73 @@
 <template>
-  <div>
-    <h1>mc</h1>
-    <NuxtLink to="/top">top</NuxtLink>
+  <div class="w-full max-w-sm md:max-w-7xl m-auto">
+    <p class="mt-5 mb-5">ホーム</p>
+    <Container>
+      <template #content>
+        <div class="p-4">
+          <!-- Header -->
+          <div class="flex items-end gap-x-3">
+            <HeaderText v-bind="headerText" />
+            <NuxtLink to="/top" class="text-yellow">
+              &gt;&gt; ルームを作成する
+            </NuxtLink>
+          </div>
+          <!-- Table -->
+          <table class="mt-3 w-full table-fixed border-collapse">
+            <thead>
+              <tr class="text-left h-0">
+                <th class="w-2/3 font-medium"></th>
+                <th class="w-1/6 font-medium"></th>
+                <th class="w-1/6"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="room in rooms"
+                :key="room.displayId"
+                class="border border-gray-400"
+              >
+                <td class="pt-2 pl-3 pb-3">{{ room.name }}</td>
+                <td>
+                  <img src="~assets/img/copy_url.svg" class="h-5 m-auto" />
+                </td>
+                <td class="text-center pr-3 text-right">編集</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </template>
+    </Container>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref } from '@vue/composition-api'
+import { HeaderText } from '~/components/01-atoms/HeaderText.vue'
+
+export interface Room {
+  name: string
+  description: string
+  displayId: string
+}
 
 export default defineComponent({
-  setup() {},
+  setup() {
+    const headerText = ref<HeaderText>({
+      text: 'Rooms',
+    })
+    const rooms = ref<Room[]>([
+      {
+        name: 'DJちっち',
+        description: '',
+        displayId: 'dj-chicchi',
+      },
+      {
+        name: 'DJさわっくま',
+        description: '',
+        displayId: 'dj-sawa-kuma',
+      },
+    ])
+    return { headerText, rooms }
+  },
 })
 </script>

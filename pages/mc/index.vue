f4b14dbc6460f5fb97a1f6c2d409c0a83f579e2c
Yo Higashida <57238213+penguin4731@users.noreply.github.com>
2022-06-22 17:41:15 +0900

    Issue 61 design-mc04 all room (#84)
    * add: copy_url
    
    * add: room
    
    * add:  comment
    
    * add: account
    
    * fix: rooms
    
    * add: music-svg
    
    * add: account
    
    * add: bg-colors
    
    * add: responsive
    
    * design: responsive
    
    * fix: room-create-url
    
    * fix: description
    
    * fix: auth
    
    * add: router
    
    * fix-design: add-name
    
    * fix: sample-description
    
    * fix: middleware

--- pages/mc/index.vue
+++ pages/mc/index.vue
@@ -1,14 +1,164 @@
 <template>
-  <div>
-    <h1>mc</h1>
-    <NuxtLink to="/top">top</NuxtLink>
+  <div class="w-full max-w-sm md:max-w-7xl m-auto">
+    <p class="mt-5 mb-5">ホーム</p>
+    <!-- Rooms -->
+    <div class="flex flex-col md:flex-row gap-10">
+      <Container>
+        <template #content>
+          <!-- Header -->
+          <div class="flex items-end gap-x-3">
+            <HeaderText v-bind="roomHeaderText" />
+            <NuxtLink to="/mc/room/create" class="text-yellow">
+              &gt;&gt; ルームを作成する
+            </NuxtLink>
+          </div>
+          <p class="mt-3">
+            "Room"ごとに曲のリクエストを募集することができます。
+          </p>
+          <!-- Table -->
+          <table class="mt-3 w-full table-fixed border-collapse">
+            <thead class="hidden md:table-header-group text-left">
+              <tr class="border border-gray-400">
+                <th class="pl-3 pt-2 pb-2">ルーム名</th>
+                <th>ルーム説明</th>
+                <th>共有URL</th>
+                <th class="w-1/6"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="room in rooms"
+                :key="room.displayId"
+                class="border border-gray-400 hover:bg-neon-blue cursor-pointer"
+                @click="routerPush(room.displayId)"
+              >
+                <td class="pt-2 pl-3 pb-3 hidden md:table-cell">
+                  {{ room.name }}
+                </td>
+                <td class="pt-2 pl-3 pb-3 md:pl-0">
+                  {{ room.description }}
+                  <div class="text-sm md:hidden">
+                    {{ room.name }}
+                  </div>
+                </td>
+                <td class="hidden md:table-cell">
+                  <div class="flex gap-1.5">
+                    <NuxtLink
+                      :to="'/room/' + room.displayId"
+                      class="underline"
+                      target="_blank"
+                    >
+                      {{ room.displayId }}
+                    </NuxtLink>
+                    <button>
+                      <img src="~/assets/img/copy_url.svg" class="w-4" />
+                    </button>
+                  </div>
+                </td>
+                <td class="text-center pr-3 text-right">＞</td>
+              </tr>
+            </tbody>
+          </table>
+        </template>
+      </Container>
+      <!-- account -->
+      <Container class="max-w-md">
+        <template #content>
+          <HeaderText v-bind="accountHeaderText" />
+          <p class="mt-3">各サービスのアカウントと連携します。</p>
+          <table class="mt-3 w-full table-auto border-collapse">
+            <thead>
+              <tr class="hidden">
+                <th class="w-2/3"></th>
+                <th class="w-1/3"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="account in accounts"
+                :key="account.name"
+                class="border border-gray-400"
+              >
+                <td class="pt-2 pl-3 pb-2 flex items-center gap-x-3">
+                  <img
+                    :src="require('@/assets/img/' + account.name + '_Icon.svg')"
+                    class="w-9"
+                  />
+                  {{ account.name }}
+                </td>
+                <td class="text-center text-center">
+                  <button v-if="account.isConnected" class="pt-2 pb-2">
+                    連携を解除
+                  </button>
+                  <button v-else class="text-yellow pt-2 pb-2">
+                    &gt;&gt; 連携する
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </template>
+      </Container>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref } from '@vue/composition-api'
+import { useRouter } from '@nuxtjs/composition-api'
+import { HeaderText } from '~/components/01-atoms/HeaderText.vue'
+
+export interface Room {
+  name: string
+  description: string
+  displayId: string
+}
+
+export interface Account {
+  name: string
+  isConnected: boolean
+}
 
 export default defineComponent({
-  setup() {},
+  setup() {
+    const router = useRouter()
+    const roomHeaderText = ref<HeaderText>({
+      text: 'Rooms',
+    })
+    const accountHeaderText = ref<HeaderText>({
+      text: 'Account Link',
+    })
+    const rooms = ref<Room[]>([
+      {
+        name: 'DJちっち',
+        description: 'A日程用',
+        displayId: 'dj-chicchi',
+      },
+      {
+        name: 'DJさわっくま',
+        description: 'B日程用',
+        displayId: 'dj-sawa-kuma',
+      },
+      {
+        name: 'DJたいちゃん',
+        description: 'スクール大阪日A',
+        displayId: 'dj-taichan',
+      },
+    ])
+    const accounts = ref<Account[]>([
+      {
+        name: 'Spotify',
+        isConnected: false,
+      },
+      {
+        name: 'AppleMusic',
+        isConnected: true,
+      },
+    ])
+    const routerPush = (displayId: String) => {
+      router.push(`/mc/room/${displayId}`)
+    }
+    return { roomHeaderText, accountHeaderText, accounts, rooms, routerPush }
+  },
 })
 </script>

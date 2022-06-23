4ff436cf25841ad5ade0d39b705a117ad317ad95
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-06-23 19:37:20 +0900

    [add] useRequestMusic

---
+++ core/03-composables/useRequestMusic.ts
@@ -0,0 +1,19 @@
+import { ref, Ref } from '@nuxtjs/composition-api'
+import { IRequestMusicRepository } from '~/core/02-repositories/requestMusic'
+import { RequestMusicParams } from '~/types/params/requestMusic'
+
+export interface UseRequestMusicInputs {
+  params: RequestMusicParams
+  roomId: string
+}
+
+export const useRequestMusic = (repository: IRequestMusicRepository) => {
+  const requestMusicResult: Ref<string | undefined> = ref(undefined)
+  const requestMusic = (inputs: UseRequestMusicInputs) => {
+    repository
+      .post(inputs)
+      .then((result) => (requestMusicResult.value = result))
+  }
+
+  return { requestMusicResult, requestMusic }
+}

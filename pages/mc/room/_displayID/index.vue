bbeff761e097c0b8cd2c170e05471778e7316475
Yo Higashida <y.higashida0226+app@gmail.com>
2022-06-23 19:40:48 +0900

    add: レスポンシブ対応

--- pages/mc/room/_displayID/index.vue
+++ pages/mc/room/_displayID/index.vue
@@ -3,78 +3,95 @@
     <p class="mt-5 mb-5">
       <NuxtLink to="/mc">ホーム</NuxtLink> / {{ room.name }}
     </p>
-    <div class="flex flex-col md:flex-row gap-10">
+    <div class="flex flex-col gap-10">
       <!-- Details -->
-      <Container>
-        <template #content>
-          <div class="flex items-end gap-x-3">
-            <HeaderText v-bind="detailTitle" />
-            <NuxtLink
-              :to="`/mc/room/${room.displayId}/settings`"
-              class="text-yellow"
-            >
-              &gt;&gt; ルームを編集する
-            </NuxtLink>
-          </div>
-          <ul class="mt-3 mb-3">
-            <li>ルームネーム: {{ room.name }}</li>
-            <li>ルーム説明: {{ room.description }}</li>
-            <li>使用サービス: {{ room.type }}</li>
-            <li>
-              ルームURL:
-              <NuxtLink :to="`/room/${room.displayId}`">{{
-                protocolAndHostname + '/room/' + room.displayId
-              }}</NuxtLink>
-            </li>
-          </ul>
-        </template>
-      </Container>
-      <!-- Request -->
-      <Container>
-        <template #content>
-          <div class="flex items-end gap-x-3">
-            <HeaderText v-bind="requestTitle" />
-            <a :href="room.playlistId" target="_blank" class="text-yellow">
-              &gt;&gt; プレイリストを見る
-            </a>
-          </div>
-          <!-- Table -->
-          <table class="mt-3 w-full border-collapse">
-            <thead class="hidden">
-              <tr>
-                <th class="w-min"></th>
-                <th class="w-full"></th>
-                <th class="w-1/6"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="music in musics"
-                :key="music.providedMusicId"
-                class="border border-gray-400 hover:bg-neon-blue cursor-pointer"
-              >
-                <td class="w-14">
-                  <img
-                    :src="music.imageUrl"
-                    class="w-14 h-14 mt-1 ml-1 mb-1 object-cover"
-                  />
-                </td>
-                <td class="pl-3 md:pl-0">
-                  {{ music.musicTitle }}
-                  <div class="text-sm md:hidden">
-                    {{ music.artistName }}
-                  </div>
-                </td>
-                <td>
-                  <a :href="music.providerUrl" target="_blank">
-                    <img src="~/assets/img/copy_url.svg" class="w-4" />
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </template>
-      </Container>
+      <div class="flex flex-col md:flex-row gap-10">
+        <div class="md:w-3/5 self-stretch">
+          <Container>
+            <template #content>
+              <div class="flex items-end gap-x-3">
+                <HeaderText v-bind="detailTitle" />
+                <NuxtLink
+                  :to="`/mc/room/${room.displayId}/settings`"
+                  class="text-yellow"
+                >
+                  &gt;&gt; ルームを編集する
+                </NuxtLink>
+              </div>
+              <ul class="mt-3 mb-3">
+                <li>ルームネーム: {{ room.name }}</li>
+                <li>ルーム説明: {{ room.description }}</li>
+                <li>使用サービス: {{ room.type }}</li>
+                <li>
+                  ルームURL:
+                  <NuxtLink :to="`/room/${room.displayId}`">{{
+                    protocolAndHostname + '/room/' + room.displayId
+                  }}</NuxtLink>
+                </li>
+              </ul>
+            </template>
+          </Container>
+        </div>
+        <!-- Request -->
+        <Container>
+          <template #content>
+            <div class="flex items-end gap-x-3">
+              <HeaderText v-bind="requestTitle" />
+              <a :href="room.playlistId" target="_blank" class="text-yellow">
+                &gt;&gt; プレイリストを見る
+              </a>
+            </div>
+            <!-- Table -->
+            <table class="mt-3 w-full border-collapse">
+              <thead class="hidden md:table-header-group text-left">
+                <tr>
+                  <th>Song</th>
+                  <th></th>
+                  <th>Artist</th>
+                  <th>Album</th>
+                  <th>Time</th>
+                  <th>RadioName</th>
+                  <th>Link</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="music in musics"
+                  :key="music.providedMusicId"
+                  class="border border-gray-400 hover:bg-neon-blue cursor-pointer w-full"
+                >
+                  <td class="w-14">
+                    <img
+                      :src="music.imageUrl"
+                      class="w-14 h-14 mt-1 ml-1 mb-1 object-cover"
+                    />
+                  </td>
+                  <td class="pl-3">
+                    {{ music.musicTitle }}
+                    <div class="text-sm md:hidden">
+                      {{ music.artistName }}
+                    </div>
+                  </td>
+                  <td class="hidden md:table-cell">{{ music.artistName }}</td>
+                  <td class="hidden md:table-cell">{{ music.albumName }}</td>
+                  <td class="hidden md:table-cell">{{ music.duration }}</td>
+                  <td class="hidden md:table-cell w-1/6 text-sm">
+                    {{ music.radioNames.join(',') }}
+                  </td>
+                  <td>
+                    <a :href="music.providerUrl" target="_blank">
+                      <img
+                        src="~/assets/img/copy_url.svg"
+                        class="w-4 ml-2 mr-2"
+                      />
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </template>
+        </Container>
+      </div>
       <!-- Message -->
       <Container>
         <template #content>
@@ -82,8 +99,8 @@
           <table class="mt-3 w-full border-collapse">
             <thead>
               <tr class="text-left">
-                <th>name</th>
-                <th>message</th>
+                <th>Name</th>
+                <th>Message</th>
               </tr>
             </thead>
             <tbody>
@@ -92,7 +109,7 @@
                 :key="letter.id"
                 class="border border-gray-400 hover:bg-neon-blue cursor-pointer"
               >
-                <td class="pl-2 md:pl-0">{{ letter.radioName }}</td>
+                <td class="pl-2 pt-2 pb-2">{{ letter.radioName }}</td>
                 <td>{{ letter.message }}</td>
               </tr>
             </tbody>
@@ -111,11 +128,13 @@ import { HeaderText } from '~/components/01-atoms/HeaderText.vue'
 
 export interface RequestMusicDetail {
   musicTitle: string
+  albumName: string
   artistName: string
   imageUrl: string
   providerUrl: string
   duration: number
   providedMusicId: string
+  radioNames: string[]
 }
 
 export interface Letter {
@@ -150,6 +169,7 @@ export default defineComponent({
     const musics = ref<RequestMusicDetail[]>([
       {
         musicTitle: 'ヒステリックナイトガール',
+        albumName: 'Styley!',
         artistName: 'PSUQUI',
         imageUrl:
           'https://i.scdn.co/image/ab67616d00001e021a8bba168d85553d9b2d47a9',
@@ -157,16 +177,19 @@ export default defineComponent({
           'https://i.scdn.co/image/ab67616d00001e021a8bba168d85553d9b2d47a9',
         duration: 220,
         providedMusicId: 'm2ed2',
+        radioNames: ['がっしー', 'てぃーてぃー'],
       },
       {
         musicTitle: 'ヒステリックナイトガール',
         artistName: 'PSUQUI',
+        albumName: 'Styley!',
         imageUrl:
           'https://i.scdn.co/image/ab67616d00001e021a8bba168d85553d9b2d47a9',
         providerUrl:
           'https://i.scdn.co/image/ab67616d00001e021a8bba168d85553d9b2d47a9',
         duration: 220,
         providedMusicId: 'm2ed2',
+        radioNames: ['がっしー'],
       },
     ])
     const letters = ref<Letter[]>([

a9dcbd4b7a053dd276f2c728d93f165ed6038eca
Yo Higashida <57238213+penguin4731@users.noreply.github.com>
2022-06-29 18:09:50 +0900

    Issue 63 feature/room create (#89)
    * add: create-room
    
    * add: select-input
    
    * add: create-room
    
    * fix: design

---
+++ components/01-atoms/SelectInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <select
+    id="pet-select"
+    v-model="selectComputed"
+    name="pets"
+    class="w-full px-3 py-4 text-gray-200 bg-gray-800 border-2 rounded-lg appearance-none transition ease-in duration-100 border-neon-blue focus:shadow-text-input-neon-blue focus:outline-none"
+  >
+    <option
+      v-for="option in selectOptions"
+      :key="option.value"
+      :value="option.value"
+    >
+      {{ option.text }}
+    </option>
+  </select>
+</template>
+
+<script lang="ts">
+import { Ref, defineComponent, computed } from '@nuxtjs/composition-api'
+
+export interface SelectInputOption {
+  value: string
+  text: string
+}
+
+export interface SelectInput {
+  selected: Ref<string>
+  options: Array<SelectInputOption>
+}
+
+export default defineComponent({
+  props: {
+    selected: { type: String, required: true },
+    options: { type: Array, required: true },
+  },
+  emits: ['update:select'],
+  setup(props: SelectInput, { emit }: any) {
+    const selectComputed = computed({
+      get: () => props.selected,
+      set: (value) => {
+        emit('update:select', value)
+      },
+    })
+    const selectOptions = props.options
+    return { selectComputed, selectOptions }
+  },
+})
+</script>
--- pages/mc/room/create/index.vue
+++ pages/mc/room/create/index.vue
@@ -1,20 +1,108 @@
 <template>
-  <div>
-    <h1>mc/room/create</h1>
-    <p>{{ id }}</p>
-    <NuxtLink to="/top">top</NuxtLink>
+  <div class="w-full max-w-sm md:max-w-7xl m-auto mb-5">
+    <p class="mt-5 mb-5">ホーム / ルーム作成</p>
+    <Container>
+      <template #content>
+        <HeaderText text="Create Room" class="mb-5 md:mb-10" />
+        <div
+          class="mb-8 grid grid-cols-1 md:grid-cols-title-and-content items-center gap-2 md:gap-5 md:gap-y-8"
+        >
+          <p>ルームネーム*</p>
+          <TextInput
+            text=""
+            placeholder="例)がっしー"
+            @update:text="updateName"
+          />
+          <p>ルーム説明*</p>
+          <TextInput
+            text=""
+            placeholder="例)2022年夏のイベント用"
+            @update:text="updateDescription"
+          />
+          <p>使用サービス*</p>
+          <SelectInput
+            v-model="musicServiceSelect.selected"
+            v-bind="musicServiceSelect"
+          />
+          <p>リクエストURL*</p>
+          <TextInput
+            text=""
+            placeholder="例)gassi"
+            @update:text="updateDisplayId"
+          />
+        </div>
+        <div class="flex justify-center mb-3">
+          <Button
+            class="mr-3 bg-gray-400 hover:bg-gray-500"
+            text="戻る"
+            @click.native="cancel"
+          />
+          <Button text="ルームを作成する" @click.native="submit" />
+        </div>
+      </template>
+    </Container>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
-import { computed, useRoute } from '@nuxtjs/composition-api'
+import {
+  computed,
+  useRoute,
+  ref,
+  defineComponent,
+  reactive,
+  toRefs,
+} from '@nuxtjs/composition-api'
+import { SelectInput } from '~/components/01-atoms/SelectInput.vue'
+
+interface State {
+  name: string
+  description: string
+  service: string
+  displayId: string
+}
 
 export default defineComponent({
   setup() {
+    const state = toRefs(
+      reactive<State>({
+        name: '',
+        description: '',
+        service: '',
+        displayId: '',
+      })
+    )
+    const selectedMusicService = ref('')
+    const musicServiceSelect = ref<SelectInput>({
+      selected: selectedMusicService,
+      options: [
+        { value: 'applemusic', text: 'AppleMusic' },
+        { value: 'spotify', text: 'Spotify' },
+      ],
+    })
+    const updateName = (name: string): void => {
+      state.name.value = name
+    }
+    const updateDescription = (description: string): void => {
+      state.description.value = description
+    }
+    const updateService = (service: string): void => {
+      state.service.value = service
+    }
+    const updateDisplayId = (displayId: string): void => {
+      state.displayId.value = displayId
+    }
     const route = useRoute()
     const id = computed(() => route.value.params.id)
-    return { id }
+    return {
+      state,
+      musicServiceSelect,
+      id,
+      updateName,
+      updateDescription,
+      updateService,
+      updateDisplayId,
+    }
   },
 })
 </script>

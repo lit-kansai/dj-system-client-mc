c66a6066c6bbfc52bce0403be280dbcaf77c1b52
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-07-12 22:46:40 +0900

    [add] LoadingOverlay component

---
+++ components/02-molecules/LoadingOverlay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="absolute inset-x-0 top-0 left-0 z-10 flex items-center justify-center w-full h-full bg-black bg-opacity-60"
+  >
+    <div class="flex items-center mb-20">
+      <Loading />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {},
+  setup() {},
+})
+</script>

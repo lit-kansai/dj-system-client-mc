775411d9d59e8a7f54d52bff77d7278661fb03da
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-07-17 03:39:36 +0900

    [update] googleLogin logic

--- core/03-composables/useGoogleLogin.ts
+++ core/03-composables/useGoogleLogin.ts
@@ -3,6 +3,8 @@ import { GoogleLoginRepository } from '~/core/02-repositories/googleLogin'
 
 export const useGoogleLogin = (): Promise<IGoogleLoginModel> => {
   const repository = new GoogleLoginRepository()
-  const REDIRECT_URL = process.env.REDIRECT_URL as string
+  const REDIRECT_URL =
+    process.env.REDIRECT_URL ??
+    'https://dj-system.lit-kansai-mentors.com/api/google/callback'
   return repository.fetch(REDIRECT_URL)
 }

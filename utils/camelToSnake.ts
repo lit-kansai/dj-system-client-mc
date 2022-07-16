da363246751b6a53f341a2b2c603aa14b3170679
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-07-16 20:56:24 +0900

    [fix] converter logic

--- utils/camelToSnake.ts
+++ utils/camelToSnake.ts
@@ -1,17 +1,24 @@
-export class CamelConverter {
-  static convert(obj: { [key: string]: any }) {
-    return Object.keys(obj).reduce(
-      (acc: { [key: string]: any }, key: string) => {
-        // 値がobjectの場合は再帰的にconvertを実行
-        if (Object.prototype.toString.call(obj[key]) === '[object Object]') {
-          acc[this.toCamel(key)] = this.convert(obj[key])
-          return acc
-        }
-        acc[this.toCamel(key)] = obj[key]
+export class CamelToSnake {
+  static convert(obj: { [key: string]: any } | any[]): any {
+    if (typeof obj !== 'object') {
+      return obj
+    }
+    if (Array.isArray(obj)) {
+      return obj.map(CamelToSnake.convert)
+    }
+    if (obj === null) {
+      return null
+    }
+    return Object.keys(obj).reduce((acc: any, key: string) => {
+      // 値がobjectの場合は再帰的にconvertを実行
+      const type = Object.prototype.toString.call(obj[key])
+      if (type === '[object Object]' || type === '[object Array]') {
+        acc[CamelToSnake.toCamel(key)] = CamelToSnake.convert(obj[key])
         return acc
-      },
-      {}
-    )
+      }
+      acc[CamelToSnake.toCamel(key)] = obj[key]
+      return acc
+    }, {})
   }
 
   // snake_caseの単語をlowerCamelに変換する

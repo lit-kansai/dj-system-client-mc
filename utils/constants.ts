8294dd2394b229a1cd6f732b58ce41909ea7a88e
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-07-19 00:02:41 +0900

    [add] SPOTIFY_LOGIN_REDIRECT_URL constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,4 @@
 export const USER_CREDENTIALS_LOCAL_STORAGE_KEY = 'userInfo'
 export const GOOGLE_LOGIN_PAGE_PATH = '/mc/login'
 export const API_GOOGLE_CALLBACK_PATH = '/api/google/callback'
+export const API_SPOTIFY_CALLBACK_PATH = '/api/spotify/callback'

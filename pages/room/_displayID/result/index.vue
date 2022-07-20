916383f26b7c9b9155a134f7079ce1c40776cdb0
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-07-20 18:33:52 +0900

    [update] room/_displayID/result/index page

--- pages/room/_displayID/result/index.vue
+++ pages/room/_displayID/result/index.vue
@@ -1,44 +1,44 @@
 <template>
   <div
-    class="w-full max-w-sm md:max-w-7xl m-auto mt-5 mb-5 md:flex md:gap-x-10"
+    class="w-full max-w-sm m-auto mt-5 mb-5 md:max-w-7xl md:flex md:gap-x-10"
   >
-    <div class="mb-5 md:w-60">
-      <HeaderText text="DJ Gassi" class="hidden md:block" />
-      <TextInput
-        v-model="textInput.text"
-        v-bind="textInput"
-        class="md:mt-3"
-        @update:text="updateSearchWord"
-      />
-    </div>
+    <Modal v-bind="modalState" @close="onCloseModal">
+      <template #content>
+        <div class="relative p-2 md:p-5">
+          <LoadingOverlay v-if="requestMusicLoading" />
+          <MemberOtayori
+            v-if="currentState === 'confirm'"
+            :music="selectedMusic"
+            @submit="onSubmit"
+          />
+          <Complete
+            v-else-if="currentState === 'finished'"
+            title="送信完了"
+            subtitle="流れるのをお楽しみに！！"
+          />
+        </div>
+      </template>
+    </Modal>
     <div class="md:w-full">
-      <Container v-if="loading">
-        <template #content>
-          <Loading />
-        </template>
-      </Container>
+      <template v-if="loading">
+        <LoadingOverlay />
+      </template>
       <template v-if="!loading">
-        <Container>
-          <template #content>
-            <div class="p-2 md:p-5">
-              <MemberMusicSelect
-                v-if="currentState === 'musicSelect'"
-                @musicSelected="musicSelected"
-              />
-              <MemberOtayori
-                v-else-if="currentState === 'confirm'"
-                :music="selectedMusic"
-                @submit="onSubmit"
-                @cancel="cancel"
-              />
-              <Complete
-                v-else-if="currentState === 'finished'"
-                title="送信完了"
-                subtitle="流れるのをお楽しみに！！"
-              />
-            </div>
-          </template>
-        </Container>
+        <TextInput
+          v-model="textInput.text"
+          v-bind="textInput"
+          class="md:mt-3"
+          @update:text="updateSearchWord"
+          @keypress.enter.native="onPressEnter"
+        />
+        <div class="mt-5">
+          <p class="text-xl">リクエストしたい曲を選択しよう！</p>
+          <MemberMusicSelect
+            class="mt-4"
+            :musics="displayMusics"
+            @musicSelected="musicSelected"
+          />
+        </div>
       </template>
     </div>
   </div>
@@ -46,13 +46,16 @@
 
 <script lang="ts">
 import {
+  ref,
   defineComponent,
   computed,
   useRoute,
   watch,
+  Ref,
 } from '@nuxtjs/composition-api'
+import { onMounted } from '@vue/composition-api'
 import {
-  useModal,
+  useModalState,
   useSelectedMusic,
 } from '~/types/pages/room/_displayID/result/index'
 import { MusicSelectedPayload } from '~/components/04-containers/MemberMusicSelect.vue'
@@ -61,27 +64,35 @@ import {
   useRequestMusic,
   UseRequestMusicInputs,
 } from '~/core/03-composables/useRequestMusic'
+import { FetchMusicRepository } from '~/core/02-repositories/fetchMusic'
+import { useFetchMusic } from '~/core/03-composables/useFetchMusic'
 import { useTextFieldComponent } from '~/core/03-composables/useTextFieldComponent'
 import { useLoading } from '~/core/03-composables/useLoading'
+import { useModal } from '~/core/03-composables/useModal'
 import { RequestMusicRepository } from '~/core/02-repositories/requestMusic'
+import { getQuery } from '~/utils/getQuery'
+import { IMusicModel } from '~/core/01-models/music'
 
 export default defineComponent({
   setup() {
     const route = useRoute()
     const displayID = computed(() => route.value.params.displayID)
+    const query = computed(() => route.value.query.searchWord)
+    const { musics, fetchMusic } = useFetchMusic(new FetchMusicRepository())
+    const displayMusics: Ref<IMusicModel[]> = ref([])
     const { searchWord, textInput, updateSearchWord } = useTextFieldComponent()
+    const { currentState, nextModal, previousModal } = useModalState()
+    const { modalState, openModal, closeModal } = useModal()
     const { loading, setLoading } = useLoading()
+    const { loading: requestMusicLoading, setLoading: setRequestMusicLoading } =
+      useLoading()
     const { selectedMusic, setSelectedMusic } = useSelectedMusic()
-    const { currentState, nextModal, previousModal } = useModal()
     const { requestMusicResult, requestMusic } = useRequestMusic(
       new RequestMusicRepository()
     )
     const musicSelected = (payload: MusicSelectedPayload) => {
       setSelectedMusic(payload.music)
-      nextModal()
-    }
-    const cancel = () => {
-      previousModal()
+      openModal()
     }
     const onSubmit = (payload: MemberOtayoriPayload) => {
       const inputs: UseRequestMusicInputs = {
@@ -92,25 +103,51 @@ export default defineComponent({
         },
         roomId: displayID.value,
       }
-      nextModal()
-      setLoading(true)
+      setRequestMusicLoading(true)
       requestMusic(inputs)
     }
-
-    watch(requestMusicResult, () => {
+    const onPressEnter = () => {
+      setLoading(true)
+      displayMusics.value = []
+      searchMusic()
+    }
+    const searchMusic = () => {
+      fetchMusic({
+        roomId: displayID.value,
+        q: searchWord.value,
+      })
+    }
+    onMounted(() => {
+      const q = getQuery(query.value)
+      updateSearchWord(q)
+      setLoading(true)
+      searchMusic()
+    })
+    const onCloseModal = () => {
+      closeModal()
+      previousModal()
+    }
+    watch(musics, (_musics) => {
+      displayMusics.value = musics.value
       setLoading(false)
     })
-
+    watch(requestMusicResult, () => {
+      setRequestMusicLoading(false)
+      nextModal()
+    })
     return {
+      modalState,
+      requestMusicLoading,
+      currentState,
+      displayMusics,
       loading,
       selectedMusic,
-      searchWord,
+      onPressEnter,
       textInput,
       updateSearchWord,
       musicSelected,
-      currentState,
       onSubmit,
-      cancel,
+      onCloseModal,
     }
   },
 })

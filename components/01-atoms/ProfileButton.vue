0b4bc3f4f9341d407aede77db6ac3bc45be798a6
Yo Higashida <57238213+penguin4731@users.noreply.github.com>
2022-07-21 18:26:33 +0900

    Headerのデザイン追加しました (#142)
    * fix: mc-header-text
    
    * add: profileButton
    
    * add: dropdown-profile
    
    * add: close
    
    * fix: close
    
    * add: redirectUrl
    
    * add: todo

---
+++ components/01-atoms/ProfileButton.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="relative">
+    <button class="flex items-center" @click="clickProfileButton">
+      <img
+        :src="imageUrl"
+        class="h-7 w-7 sm:h-8 sm:w-8 rounded-full object-cover"
+      />
+      <div
+        class="h-2 w-2 ml-2 mt-1 shrink-0 text-white group-hover:text-gray-200"
+      >
+        <svg
+          class="fill-current"
+          viewBox="0 0 8 6"
+          fill="white"
+          xmlns="http://www.w3.org/2000/svg"
+          :class="isOpen ? 'rotate-180' : ''"
+        >
+          <path d="M4.21053 5.05263L8 0H0.421053L4.21053 5.05263Z"></path>
+        </svg>
+      </div>
+    </button>
+    <div
+      v-if="isOpen"
+      class="fixed top-0 bottom-0 left-0 right-0"
+      @click="close()"
+    />
+    <div v-if="isOpen" class="absolute right-0 z-10">
+      <div
+        class="mt-2 bg-gray-800 w-80 rounded-2xl border-2 border-neon-blue shadow-modal-neon-blue z-30"
+      >
+        <div class="flex flex-col p-5">
+          <div class="flex items-center">
+            <img
+              :src="imageUrl"
+              class="h-7 w-7 sm:h-8 sm:w-8 rounded-full object-cover"
+            />
+            <div class="pl-3">
+              <p class="font-bold">{{ userName }}</p>
+              <p>{{ email }}</p>
+            </div>
+          </div>
+          <div class="border-t border-gray-400 mt-4 mb-4" />
+          <button class="text-left cursor-pointer">Logout</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export interface ProfileButton {
+  imageUrl: string
+  userName: string
+  email: string
+}
+
+export default defineComponent({
+  props: {
+    imageUrl: { type: String, required: true },
+    userName: { type: String, required: true },
+    email: { type: String, required: true },
+  },
+  setup() {
+    const isOpen = ref(false)
+    const clickProfileButton = () => {
+      isOpen.value = !isOpen.value
+    }
+    const close = () => {
+      isOpen.value = false
+    }
+    return {
+      isOpen,
+      clickProfileButton,
+      close,
+    }
+  },
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="relative flex flex-col h-screen min-h-screen">
-    <Header v-bind="header">
+    <Header v-bind="state.haeder.value">
       <template #right>
-        <p class="hidden md:block">powered by DJ GASSI</p>
+        <ProfileButton
+          v-if="state.isShowProfileButton.value"
+          v-bind="state.profile.value"
+        />
+        <p v-else class="hidden md:block">powered by DJ GASSI</p>
       </template>
     </Header>
     <div class="flex-grow">
@@ -12,16 +16,52 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import {
+  defineComponent,
+  onBeforeMount,
+  reactive,
+  toRefs,
+  useRoute,
+} from '@nuxtjs/composition-api'
+import { ProfileButton } from '~/components/01-atoms/ProfileButton.vue'
 import { Header } from '~/components/03-organisms/Header.vue'
+
+interface State {
+  haeder: Header
+  isShowProfileButton: boolean
+  profile: ProfileButton
+}
 
 export default defineComponent({
   setup() {
-    const header = ref<Header>({
-      title: 'DJ Gassi',
+    const route = useRoute()
+    const state = toRefs(
+      reactive<State>({
+        haeder: { title: 'DJ Gassi', redirectUrl: '' },
+        isShowProfileButton: false,
+        profile: {
+          imageUrl: 'https://guide.line.me/ja/dogday_01.jpg',
+          userName: 'Dog',
+          email: 'dog@sample.com',
+        },
+      })
+    )
+    onBeforeMount(() => {
+      const currentPath = route.value.path
+      const mcPageRegex = /^(\/mc.*)/
+
+      if (mcPageRegex.test(currentPath)) {
+        state.isShowProfileButton.value = true
+        state.haeder.value.title = 'DJ Gassi Console'
+        state.haeder.value.redirectUrl = '/mc'
+        // TODO: MCの個人情報を取得する
+      } else {
+        state.isShowProfileButton.value = false
+        // TODO: メンバーページの場合のタイトルとホームのアドレスを設定
+      }
     })
     return {
-      header,
+      state,
     }
   },
 })

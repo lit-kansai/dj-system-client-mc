787d2131f8d0cc57e60b3f3d32969e03507c9608
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-07-21 22:39:53 +0900

    [update] room/_displayID/index page

--- pages/room/_displayID/index.vue
+++ pages/room/_displayID/index.vue
@@ -1,16 +1,26 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-center w-full max-w-sm m-auto h-5/6 md:max-w-7xl md:flex-row md:gap-x-10"
-  >
-    <RoomLogo v-bind="roomLogo" class="md:max-w-[500px]" />
-    <TextInput
-      v-model="textInput.text"
-      v-bind="textInput"
-      class="mt-5 md:max-w-lg"
-      @update:text="updateSearchWord"
-      @keypress.enter.native="submit"
-    />
-    <Button class="mt-5" text="検索する" @click.native="submit" />
+  <div v-if="loading">
+    <LoadingOverlay />
+  </div>
+  <div v-else>
+    <div
+      class="flex flex-col items-center justify-center w-full max-w-sm m-auto h-5/6 md:max-w-7xl md:flex-row md:gap-x-10"
+    >
+      <div v-if="roomNotFound">
+        <p>ルームが見つかりませんでした</p>
+      </div>
+      <template v-else>
+        <RoomLogo v-bind="roomLogo" class="md:max-w-[500px]" />
+        <TextInput
+          v-model="textInput.text"
+          v-bind="textInput"
+          class="mt-5 md:max-w-lg"
+          @update:text="updateSearchWord"
+          @keypress.enter.native="submit"
+        />
+        <Button class="mt-5" text="検索する" @click.native="submit" />
+      </template>
+    </div>
   </div>
 </template>
 
@@ -21,38 +31,60 @@ import {
   computed,
   useRoute,
   useRouter,
+  onMounted,
+  watch,
 } from '@nuxtjs/composition-api'
-import { TextInput } from '~/types/components/textInput'
 import { RoomLogo } from '~/components/02-molecules/RoomLogo.vue'
+import { FetchRoomOverviewRepository } from '~/core/02-repositories/fetchRoomOverview'
+import { useFetchRoomOverview } from '~/core/03-composables/useFetchRoomOverview'
+import { useTextField } from '~/core/03-composables/useTextField'
 
 export default defineComponent({
-  layout: 'default',
   setup() {
-    const route = useRoute()
-    const router = useRouter()
-    const id = computed(() => route.value.params.displayID)
-    const roomLogo = ref<RoomLogo>({
-      roomName: 'ルーム名',
-      imageUrl: '',
-    })
-    const searchWord = ref('')
-    const textInput = ref<TextInput>({
-      text: searchWord,
+    const {
+      loading,
+      fetchRoomOverviewResponse,
+      roomNotFound,
+      fetchRoomOverviewError,
+      mounted,
+    } = useFetchRoomOverview(new FetchRoomOverviewRepository())
+    const displayID = computed(() => useRoute().value.params.displayID)
+    const { text, textInput, updateSearchWord } = useTextField({
       type: 'text',
       placeholder: '曲名・アーティスト名を入力',
       spellcheck: false,
       autocomplete: 'off',
     })
-    const updateSearchWord = (word: string) => {
-      searchWord.value = word
-    }
+    const roomLogo = ref<RoomLogo>({
+      roomName: '',
+      imageUrl: '',
+    })
     const submit = () => {
+      const router = useRouter()
       router.push({
-        path: `/room/${id.value}/result`,
-        query: { searchWord: searchWord.value },
+        path: `/room/${displayID.value}/result`,
+        query: { searchWord: text.value },
       })
     }
-    return { id, roomLogo, searchWord, submit, textInput, updateSearchWord }
+    watch(fetchRoomOverviewResponse, (value) => {
+      if (value) {
+        roomLogo.value.roomName = value.name
+      }
+    })
+    watch(fetchRoomOverviewError, (error) =>
+      alert(`ルームの取得に失敗しました。${JSON.stringify(error)}`)
+    )
+    onMounted(() => {
+      mounted({ roomId: displayID.value })
+    })
+    return {
+      loading,
+      roomNotFound,
+      roomLogo,
+      submit,
+      textInput,
+      updateSearchWord,
+    }
   },
 })
 </script>

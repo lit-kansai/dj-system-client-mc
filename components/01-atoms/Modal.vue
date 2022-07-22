916c95edcd94dca82eaa8063133d94557b761a0e
Yo Higashida <y.higashida0226+app@gmail.com>
2022-07-22 22:29:50 +0900

    fix: modal

--- components/01-atoms/Modal.vue
+++ components/01-atoms/Modal.vue
@@ -1,22 +1,24 @@
 <template>
   <div
     v-if="opened"
-    class="fixed top-0 bottom-0 left-0 right-0 z-20 flex items-center justify-center bg-black animate-fade-in-down transform bg-opacity-40"
+    class="fixed overflow-y-auto top-0 bottom-0 left-0 right-0 z-20 bg-black animate-fade-in-down transform bg-opacity-40 pt-10 pb-10"
     @click="close()"
   >
-    <div
-      class="w-[95%] max-w-sm mr-6 ml-6 md:max-h-[80%] md:max-w-6xl relative z-30 p-3.5 md:p-5 border-2 opacity-100 border-neon-blue shadow-modal-neon-blue rounded-2xl"
-    >
-      <ModalCloseButton
-        class="absolute top-0 right-0 transform origin-center translate-x-1/2 -translate-y-1/2"
-        @click.native="close()"
-      />
+    <div class="flex items-center justify-center">
       <div
-        class="border-2 border-neon-pink shadow-modal-neon-pink rounded-2xl"
-        @click.stop=""
+        class="w-[95%] max-w-sm md:max-w-6xl relative z-30 m-6 p-3.5 md:p-5 border-2 opacity-100 rounded-2xl border-neon-blue shadow-modal-neon-blue"
       >
-        <div class="bg-gray-800 rounded-2xl">
-          <slot name="content"> </slot>
+        <ModalCloseButton
+          class="absolute top-0 right-0 transform origin-center translate-x-1/2 -translate-y-1/2"
+          @click.native="close()"
+        />
+        <div
+          class="border-2 border-neon-pink shadow-modal-neon-pink rounded-2xl"
+          @click.stop=""
+        >
+          <div class="bg-gray-800 rounded-2xl">
+            <slot name="content"> </slot>
+          </div>
         </div>
       </div>
     </div>

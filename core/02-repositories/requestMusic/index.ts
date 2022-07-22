87936008c1a8bb394d1e48bada71b34f781ecfdc
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-07-22 21:29:42 +0900

    [update] requestMusic data flow

--- core/02-repositories/requestMusic/index.ts
+++ core/02-repositories/requestMusic/index.ts
@@ -1,16 +1,16 @@
 import { UseRequestMusicInputs } from '~/core/03-composables/useRequestMusic'
+import { $axios } from '~/utils/api'
 
 export interface IRequestMusicRepository {
-  post(params: UseRequestMusicInputs): Promise<string>
+  post(inputs: UseRequestMusicInputs): Promise<{ ok: boolean }>
 }
 
 export class RequestMusicRepository implements IRequestMusicRepository {
-  post(params: UseRequestMusicInputs): Promise<string> {
-    return new Promise((resolve) => {
-      console.log(params)
-      setTimeout(() => {
-        resolve(`success: ${Date.now()}`)
-      }, 500)
+  post(inputs: UseRequestMusicInputs): Promise<{ ok: boolean }> {
+    const { roomId, params } = inputs
+    return $axios.$post(`/room/${roomId}/request`, {
+      ...params,
+      convert: true,
     })
   }
 }

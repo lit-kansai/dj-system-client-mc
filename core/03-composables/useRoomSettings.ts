db02ab24bb30dc1b6a361dc0748dd9e3e3c5d93f
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-07-24 19:09:52 +0900

    [add] useRoomSettings

---
+++ core/03-composables/useRoomSettings.ts
@@ -0,0 +1,57 @@
+import { ref, Ref } from '@nuxtjs/composition-api'
+import { AxiosError } from 'axios'
+import { IFetchRoomDetailRepository } from '~/core/02-repositories/fetchRoomDetail'
+import { IUpdateRoomRepository } from '~/core/02-repositories/updateRoom'
+import {
+  IRoomDetail,
+  toRoomDetailComponentProps,
+} from '~/types/components/roomDetail'
+import { useLoading } from '~/core/03-composables/useLoading'
+import { IFetchRoomDetailParams } from '~/types/params/fetchRoomDetail'
+import { IUpdateRoomParams } from '~/types/params/updateRoom'
+
+export const useRoomSettings = (
+  fetchRoomDetailRepository: IFetchRoomDetailRepository,
+  updateRoomRepository: IUpdateRoomRepository
+) => {
+  const { loading: useRoomSettingsLoading, setLoading } = useLoading()
+  const fetchRoomSettingsError = ref<undefined | AxiosError | Error>(undefined)
+  const roomDetail: Ref<IRoomDetail> = ref({
+    name: '',
+    description: '',
+    displayId: '',
+    type: '',
+    playlistId: '',
+  })
+
+  const fetchRoomDetail = async (params: IFetchRoomDetailParams) => {
+    try {
+      setLoading(true)
+      const roomDetailResponse = await fetchRoomDetailRepository.get(params)
+      roomDetail.value = toRoomDetailComponentProps(roomDetailResponse)
+    } catch (error) {
+      fetchRoomSettingsError.value = error
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const updateRoomDetail = async (params: IUpdateRoomParams) => {
+    try {
+      setLoading(true)
+      await updateRoomRepository.put(params)
+    } catch (error) {
+      fetchRoomSettingsError.value = error
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {
+    useRoomSettingsLoading,
+    fetchRoomSettingsError,
+    roomDetail,
+    fetchRoomDetail,
+    updateRoomDetail,
+  }
+}

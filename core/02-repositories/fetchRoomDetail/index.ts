092569b41532668b2fd1ab7eab1bca928ae4767a
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-08-03 17:28:35 +0900

    [add] fetchRoomDetail repository

--- core/02-repositories/fetchRoomDetail/index.ts
+++ core/02-repositories/fetchRoomDetail/index.ts
@@ -1,18 +1,22 @@
-import { IFetchRoomDetailModel } from '~/core/01-models/fetchRoomDetail'
+import {
+  IRoomDetailEntity,
+  IRoomDetailModel,
+  RoomDetailModel,
+} from '~/core/01-models/roomDetail'
 import { IFetchRoomDetailParams } from '~/types/params/fetchRoomDetail'
 import { $axios } from '~/utils/api'
 
 export interface IFetchRoomDetailRepository {
-  get(params: IFetchRoomDetailParams): Promise<IFetchRoomDetailModel>
+  get(params: IFetchRoomDetailParams): Promise<IRoomDetailModel>
 }
 
 export class FetchRoomDetailRepository implements IFetchRoomDetailRepository {
-  async get(params: IFetchRoomDetailParams): Promise<IFetchRoomDetailModel> {
+  async get(params: IFetchRoomDetailParams): Promise<IRoomDetailModel> {
     const roomId = params.roomId
     const result = await $axios
       .$get(`/mc/room/${roomId}`)
-      .then((res: IFetchRoomDetailModel) => {
-        return res
+      .then((res: IRoomDetailEntity) => {
+        return new RoomDetailModel(res)
       })
     return result
   }

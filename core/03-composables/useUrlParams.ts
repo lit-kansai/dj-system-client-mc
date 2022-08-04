d661c20dc81eebbaf16e2eb6b901ff6b7723a1eb
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-08-04 15:30:26 +0900

    [add] useUrlParams

---
+++ core/03-composables/useUrlParams.ts
@@ -0,0 +1,29 @@
+import { ref, Ref } from '@nuxtjs/composition-api'
+import { Dictionary } from '~/types/data/dictionary'
+import { UrlParamsKey } from '~/types/data/urlParamsKey'
+import { hasProperty } from '~/utils/hasProperty'
+
+interface IUseUrlParams {
+  setParams(_params: Dictionary<string>): void
+  getParams(key: UrlParamsKey): string
+}
+
+const useUrlParams = (): IUseUrlParams => {
+  const params: Ref<Dictionary<string>> = ref({})
+  const setParams = (_params: Dictionary<string>) => {
+    params.value = _params
+  }
+  const getParams = (key: UrlParamsKey): string => {
+    if (!hasProperty(params.value, key)) {
+      return ''
+    }
+
+    return params.value[key]
+  }
+  return {
+    getParams,
+    setParams,
+  }
+}
+
+export const sharedUseUrlParams = useUrlParams()

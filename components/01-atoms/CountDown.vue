6d9090d48af40e52dc1f3d979b5c6ace7dbe3ffd
Yo Higashida <y.higashida0226+app@gmail.com>
2022-08-13 16:05:16 +0900

    add: countdown

---
+++ components/01-atoms/CountDown.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="max-w-max text-center text-neon-pink drop-shadow-neon-pink">
+    <p class="text-7xl">{{ time }}</p>
+    <p class="tracking-wide">{{ text }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    time: { type: String, required: true },
+    text: { type: String, required: true },
+  },
+  setup() {},
+})
+</script>

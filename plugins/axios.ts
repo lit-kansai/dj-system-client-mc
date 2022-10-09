8443b01fc8029240fa73b74a2670fe16a10ce186
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-10-09 14:37:34 +0900

    [refactor] axios plugin

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -2,27 +2,29 @@ import { Context } from '@nuxt/types'
 import { CamelToSnake } from '~/utils/camelToSnake'
 import { initializeAxios } from '~/utils/api'
 import { SnakeToCamel } from '~/utils/snakeToCamel'
-import { useLocalStorageUserCredentials } from '~/core/03-composables/useLocalStorageUserCredentials'
-import { UserCredentialsRepository } from '~/core/02-repositories/UserCredentials'
-import { useUserCredentials } from '~/core/03-composables/useUserCredentials'
+import { LocalUserCredentialsRepository } from '~/core/02-repositories/localUserCredentials'
+import { useLocalUserCredentials } from '~/core/03-composables/useLocalUserCredentials'
 
 export default function ({ $axios, redirect }: Context) {
-  $axios.onRequest(async (request) => {
+  $axios.onRequest((request) => {
+    const API_TOKEN_HEADER = 'api-token'
     if (request.data && request.data?.convert) {
       const data = SnakeToCamel.convert(request.data)
       delete data.convert
       request.data = data
     }
     // TODO: 出来たらtokenが必要なエンドポイントだけ場合分けしてheaderに乗せたい
-    const { fetch } = useLocalStorageUserCredentials(
-      new UserCredentialsRepository()
+    const { fetch } = useLocalUserCredentials(
+      new LocalUserCredentialsRepository()
     )
-    const userInfo = await fetch()
-    if (userInfo) {
-      const { apiToken } = userInfo
-      $axios.setHeader('api-token', apiToken)
+    const userInfo = fetch()
+    if (!userInfo) {
+      delete request.headers?.common[API_TOKEN_HEADER]
+      return
     }
-    $axios.setHeader('Content-Type', 'application/json')
+    const { apiToken } = userInfo
+    request.headers.common[API_TOKEN_HEADER] = apiToken
+    request.headers.common['Content-Type'] = 'application/json'
     return request
   })
 
@@ -37,8 +39,10 @@ export default function ({ $axios, redirect }: Context) {
     if (error.response?.status === 401) {
       error.message =
         'ログインの有効期限が切れました。もう一度ログインし直してください'
-      const { clearUserCredentials } = useUserCredentials()
-      const ok = clearUserCredentials()
+      const { clear } = useLocalUserCredentials(
+        new LocalUserCredentialsRepository()
+      )
+      const ok = clear()
       if (!ok) {
         error.message += '\n can not clear user credentials in local storage'
       }
